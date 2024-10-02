60b49a7b9b4f8557ee7e8dce989a990152d1433c
Gabriel Cavalheiro Barreto <ga3-cavalheiro@hotmail.com>
2024-10-02 07:53:45 -0300

    adiciona deploy da aplicação

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World 4")
+import os
+import pkg_resources
+
+
+installed_packages = pkg_resources.working_set
+installed_packages_list = sorted(
+    ["%s==%s" % (i.key, i.version) for i in installed_packages]
+)
+
+print(
+    {
+        "current_version": os.getenv("CURRENT_VERSION"),
+        "dependencies": installed_packages_list
+    }
+)
